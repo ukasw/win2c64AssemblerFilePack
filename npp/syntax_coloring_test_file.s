2dc0badf7bacb08b31593d4e799dd3ab283bc672
Lukasz Wdowiarski <lukasz.wdowiarski@gmail.com>
2016-08-03 01:28:06 +0200

    add: syntax coloring test files

---
+++ npp/syntax_coloring_test_file.s
@@ -0,0 +1,24 @@
+; syntax coloring test file
+
+; comment symbols
+; 
+!
+
+; directives
+.align .byte .ds .equ .include .org .word
+
+; operators
+( ) + - > < @ # ,
+
+; opcodes
+adc and asl bcc bcs beq bit bmi bne bpl brk bvc bvs clc
+cld cli clv cmp cpx cpy dec dex dey eor inc inx iny jmp
+jsr lda ldx ldy lsr nop ora pha php pla plp rol ror rti
+rts sbc sec sed sei sta stx sty tax tay tsx txa txs tya
+
+; undocumented opcodes
+alr anc arr aso axa axs dcm hlt ins las lax
+lse oal rla rra sax say skb skw tas xaa xas
+
+; labels
+label
